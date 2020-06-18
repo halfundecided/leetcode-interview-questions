616011c5b9314be7c7c9aa542463809f6f1c031f
Mijeong Ban <mban1@stevens.edu>
2020-06-18 15:50:38 -0400

    061820

--- 389.find-the-difference.py
+++ 389.find-the-difference.py
@@ -8,9 +8,27 @@
 
 
 class Solution:
+    # My 1st Solution: Two dictionaries
     def findTheDifference(self, s: str, t: str) -> str:
         dicS = {}
         dicT = {}
         for i in range(len(s)):
+            if s[i] in dicS:
+                dicS[s[i]] += 1
+            else:
+                dicS[s[i]] = 1
+        for i in range(len(t)):
+            if t[i] in dicT:
+                dicT[t[i]] += 1
+            else:
+                dicT[t[i]] = 1
 
-            # @lc code=end
+        differences = set()
+        if len(dicT) >= len(dicS):
+            differences = dicT.items() - dicS.items()
+        else:
+            differences = dicS.items() - dicT.items()
+
+        return list(differences)[0][0]
+
+        # @lc code=end
