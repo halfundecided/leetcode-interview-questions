4edaf6d4349f05e0b11fd2f4ff4e5e62d76d6348
Mijeong Ban <mban1@stevens.edu>
2020-06-19 13:12:01 -0400

    061920

--- 389.find-the-difference.py
+++ 389.find-the-difference.py
@@ -7,28 +7,52 @@
 # @lc code=start
 
 
+# class Solution:
+#     # My 1st Solution: Two dictionaries
+#     def findTheDifference(self, s: str, t: str) -> str:
+#         dicS = {}
+#         dicT = {}
+#         for i in range(len(s)):
+#             if s[i] in dicS:
+#                 dicS[s[i]] += 1
+#             else:
+#                 dicS[s[i]] = 1
+#         for i in range(len(t)):
+#             if t[i] in dicT:
+#                 dicT[t[i]] += 1
+#             else:
+#                 dicT[t[i]] = 1
+
+#         differences = set()
+#         if len(dicT) >= len(dicS):
+#             differences = dicT.items() - dicS.items()
+#         else:
+#             differences = dicS.items() - dicT.items()
+
+#         return list(differences)[0][0]
+
+
+from collections import defaultdict
+
+
 class Solution:
-    # My 1st Solution: Two dictionaries
+    # Kevin's Solution: https://www.youtube.com/watch?v=Ds4Kvd_xn4w&list=PLi9RQVmJD2fYXgBAUfaN5MXgYPUTbzqeb&index=21
     def findTheDifference(self, s: str, t: str) -> str:
-        dicS = {}
-        dicT = {}
-        for i in range(len(s)):
-            if s[i] in dicS:
-                dicS[s[i]] += 1
+        dicS = defaultdict(int)
+        for i in s:
+            if i in dicS:
+                dicS[i] += 1
             else:
-                dicS[s[i]] = 1
-        for i in range(len(t)):
-            if t[i] in dicT:
-                dicT[t[i]] += 1
+                dicS[i] = 1
+        print(dicS)
+
+        for i in t:
+            if (i in dicS and dicS[i] == 0) or (i not in dicS):
+                return i
             else:
-                dicT[t[i]] = 1
+                dicS[i] -= 1
 
-        differences = set()
-        if len(dicT) >= len(dicS):
-            differences = dicT.items() - dicS.items()
-        else:
-            differences = dicS.items() - dicT.items()
+        return ''
 
-        return list(differences)[0][0]
 
-        # @lc code=end
+# @lc code=end
